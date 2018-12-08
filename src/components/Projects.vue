b41862d05301c17b815c00bcf572a96dde11389a
DESKTOP-62774D9\JakeSon <honolrul98@gmail.com>
2018-12-08 15:44:06 +0900

    projects 15시 44분

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -6,12 +6,12 @@
         여러가지 서비스들을 소개합니다
       </v-flex>
     </v-layout>
-    <v-layout v-for="(project,index) in projects" pa-1 :key="index">
+    <v-layout v-for="(project,index) in projects" ma-5 pa-1 :key="index">
       <div data-scroll>
         <v-layout wrap text-xs-center class="prj-projects" >
           <v-flex class="prj-infos" xs12 md7 pa-4 text-xs-left>
             <p class="prj-name">{{project.name}}</p>
-            <p class="prj-text">{{project.text}}</p>
+            <p class="prj-text" v-html="project.text"></p>
             <p class="prj-creator">만든이: {{project.creator}}</p>
             <a class="prj-url" v-if="project.url" v-bind:href="project.url">
               {{project.url}}
@@ -55,42 +55,42 @@ export default {
       projects:[
         {
           name: '스꾸터',
-          text: '이제 우리학교에도 공식 커뮤니티 있다! 성대생의 성대생을 위한 생대생에 의한 커뮤니티!',
+          text: '이제 우리학교에도 공식 커뮤니티 있다! <br>성대생의 성대생을 위한 생대생에 의한 커뮤니티!',
           creator: '유장홍, 전태훈',
           url:"https://skkuter.com",
          src: 'skkuter.png'
         },
         {
           name: '스클럽(SKKLUB)',
-          text: '성균관대학교에 있는 동아리, 학회, 소모임의 집합체! 입학해서 뭘 할지 모르는 신입생들, 복학해서 새로운 친구 사귀고 싶은 복학생들 모두 여기 참고하세요!',
+          text: '성균관대학교에 있는 동아리, 학회, 소모임의 집합체! <br>입학해서 뭘 할지 모르는 신입생들, 복학해서 새로운 친구 사귀고 싶은 복학생들 모두 여기 참고하세요!',
           creator: '이동엽, 정유진, 황희조',
           url:"https://sskklub.herokuapp.com/",
           src: 'skklub.png'
         },
         {
           name: '성균공약모음',
-          text: '성균관대학교의 2014년부터 2017년까지의 총학생회, 단과대 학생회가 어떤 공약을 내걸었는지, 얼마나 시행했는지를 확인할 수 있습니다.(성대신문 기사 내용을 바탕으로 선정)', 
+          text: '성균관대학교의 2014년부터 2017년까지의 총학생회, 단과대 학생회가 어떤 공약을 내걸었는지, 얼마나 시행했는지를 확인할 수 있습니다.<br>(성대신문 기사 내용을 바탕으로 선정)', 
           creator: '이동건, 심시은, 김준엽, 이진호',
           url:"http://skkupledgechaser.com/",
           src: 'skkupledge.png'
         },
         {
           name: '쁠모아',
-          text: '이왕 먹을거 더 많이 먹자! 마트, 편의점 1+1, 2+1 행사를 한 눈에 볼 수 있도록 해주는 웹서비스(데이터 수집 자동화 종료)',
+          text: '이왕 먹을거 더 많이 먹자! 마트, 편의점 1+1, 2+1 행사를 한 눈에 볼 수 있도록 해주는 웹서비스<br>(데이터 수집 자동화 종료)',
           creator: '권우정, 송예균, 이진호, 이동건',
           url:"https://plusmoa.herokuapp.com/",
           src: 'plusmoa.png'
         },
         {
           name: '성균관대학교 스쿨버스 앱',
-          text: '성균관대학교 스쿨버스를 마냥 추위와 더위속에서 기다리지 않아도 된다! 정류장마다 버스가 언제 오는지, 지금 몇대의 버스가 어디에 있는지 알 수 있는 서비스!',
+          text: '성균관대학교 스쿨버스를 마냥 추위와 더위속에서 기다리지 않아도 된다! <br>정류장마다 버스가 언제 오는지, 지금 몇대의 버스가 어디에 있는지 알 수 있는 서비스!',
           creator: '이종원, 고윤서, 김정환, 김주상, 전태훈',
           url:'https://skkoolbus.com',
           src: 'skkool.png'
         },
         {
           name: 'YOU 학생',
-          text: '성균관대 내 외국인들을 위한 커뮤니티.(운영시간, 학교 공지사항, 같은나라 학생들의 모임 등을 제공) P.S-코드파일만 있음.',
+          text: '성균관대 내 외국인들을 위한 커뮤니티.<br>(운영시간, 학교 공지사항, 같은나라 학생들의 모임 등을 제공) <br>P.S-코드파일만 있음.',
           creator: '김준엽, 허용회, 김경헌, 최재필',
           url:"https://github.com/Yupjun/youhaksaeng",
           src: 'skkudent.png'
@@ -129,12 +129,12 @@ export default {
   }
   [data-scroll="in"] {
     opacity: 1;
-    -webkit-transform: translateX(0);
+    /* -webkit-transform: translateX(0); */
     transform: translateX(0)
   }
   [data-scroll="out"] {
     opacity: 0;
-    -webkit-transform: translateX(-200px);
+    /* -webkit-transform: translateX(-200px); */
     transform: translateX(-200px)
   }
   .prj-name{
