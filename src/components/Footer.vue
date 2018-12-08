bf66abe228fff4cd06df1ea64d588e6108549fd1
young <youngerous@gmail.com>
2018-12-09 01:39:11 +0900

    fixed footer, add developers' name

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,6 +3,7 @@
     class="mp-footer"
     height="auto"
     color="#0c8040"
+    fixed
   >
     <v-layout justify-center row wrap>
       <v-btn
@@ -15,13 +16,22 @@
       >
         {{ link.text }}
       </v-btn>
-
+ 
       <v-btn color="white" flat round target="_blank"
       href="https://www.facebook.com/likelionskku">
           Facebook
       </v-btn>
 
+
+
     </v-layout>
+
+    <v-tooltip top>
+      <v-btn slot="activator" color="white" flat light round>
+        LikeLion 6th
+      </v-btn>
+      <span>강동헌 김동민 김병헌 김보민 김탁영 김태우 문미현 박성우 배경훈 손은호 송영민 이선경 이소윤 이찬종 최준혁</span>
+    </v-tooltip>
   </v-footer>
 </template>
 
